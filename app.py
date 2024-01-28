4a58e54145befdb459945aee5609126d9b5ff95d
Shrirang <shrirang.sapate95@gmail.com>
2024-01-28 14:25:07 +0530

    libarary updates

--- app.py
+++ app.py
@@ -41,8 +41,7 @@ def store_data(input_text, pdf_content):
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
         poppler_path="venv/Library/bin/poppler.dll"
-        print("poppler path:",poppler_path)
-        images=pdf2image.convert_from_bytes(uploaded_file.read(),poppler_path=poppler_path)
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
 
         first_page=images[0]
 
