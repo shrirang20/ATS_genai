12f1aed78ff3c21ad94a7c8d40a5081ef5656ab6
Shrirang <shrirang.sapate95@gmail.com>
2024-01-30 19:36:44 +0530

    prompt

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_response(input,pdf_content,prompt):
+def get_gemini_response(option,input,pdf_content,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
-    response=model.generate_content([input,pdf_content[0],prompt])
+    response=model.generate_content([option,input,pdf_content[0],prompt])
     return response.text
 
 def input_pdf_setup(uploaded_file):
@@ -36,18 +36,20 @@ def input_pdf_setup(uploaded_file):
                 "data": base64.b64encode(img_byte_arr).decode()
             }
         ]
-        # logging.debug(f"Poppler path: {poppler_path}")
         return pdf_parts
     else:
         raise FileNotFoundError("No file uploaded")
 
 ## StreamLit App
 
-# Set the page config with custom JavaScript code
-
-# st.components.v1.html("google_analytics.html") 
-
 st.header("ATS Tracking System")
+option = st.selectbox(
+    'Select the Job profile ou are applying for...',
+    ("Data Science", "Full Stack Web Development", "Big Data Engineering", "DEVOPS", "Data Analyst", "Data Visualisation"), 
+    index=None,
+    placeholder="Select Job profile..."
+     )
+st.write('You are applying for', option, 'role')
 input_text=st.text_area("Job Descriptiion: ", key="input")
 uploaded_file=st.file_uploader("Upload your Resume (in PDF)", type=["pdf"])
 
@@ -59,75 +61,67 @@ submit1 = st.button("Tell me about the Resume")
 submit2 = st.button("Percentage Match")
 
 input_prompt1= """
-You are an experienced HR with Tech Expirence in the field of any one job role from Data Science, Full Stack Web Development, Big Data
-Engineering, DEVOPS, Data Analyst, Data Visualisation, your task is to review the provided resume against the job
-description for these profiles.
+You are an experienced HR with Tech Expirence in the field of {option}, your task is to review the provided {uploaded_file} against the {input_text} for {option} profiles only.
+If you get any other {input_text} that doesn'tmacthes the {option},please tell that the Job profile and Job Description doesn't matches, that's it.
+If you get any other {uploaded_file} that doesn't macthes the {option},please tell that the Resume is not for {option}, that's it.
+If you get any other {input_text} that aligns with {option},only and only then do the following
 Please share your professional evaluation on whether the candidate's profile aligns with the role.
 Higlight the strengths and weaknesses of the application in realtion to the specific job requirmnets.
 """
 input_prompt2= """
-You are an skilled ATS (Application Tracking System) scanner with a deep understanding of any one job role Data Science, Full 
-Stack Web Development, Big Data Engineering, DEVOPS, Data Analyst, Data Visualisation and deep ATS functionality,
-your task is to review the provided resume against the job description for these profiles. Give the percentage match
-if the resume matches the job description. 
-First outcome should come as percentage and then the keyword missing,
-Strength of the resume based in their skills in percentage. List the skills and how much relavant they are with JD,
-Also point out the specifics words/lines/section/placing from the resume to change (if any),
-top 5 things that candidate should add in his/her resume to get hired, 
+You are an skilled ATS (Application Tracking System) scanner with a deep understanding of any one job role {option} and deep ATS functionality,
+your task is to review the provided resume against the {input_text} for {option} profile only.First line should Give the percentage match bewtween the {input_text} and {uploaded_file}, then 
+please check the following, 
+If you get any other {input_text} doesn'tmacthes the {option},please tell that the Job profile and Job Description doesn't matches, that's it.
+If you get any other {uploaded_file} doesn't macthes the {option},please tell that the resume is not for {option}, that's it.
+If you get any other {input_text} that aligns with {option} only and only then do the following,
+
+If the resume matches the job description, then provide the list keyword missing which can help to clear the ATS,then
+Strength of the resume based on their skills in percentage.
+Then List the skills and how much relavant they are with Job description,
+then point out the specifics words/lines/section/placing from the resume to change (if any), then
+top 5 things that candidate should add in his/her resume to get hired, then
 5 job description domain related projects to add to resume that might make candidate stand out 
 and last the final thoughts. 
+
+Also don't give generic results, the result should be strictly in correlation with the {input_text} and {uploaded_file}
 """
 
-if submit1:
+if submit1 :
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        response=get_gemini_response(option,input_prompt1,pdf_content,input_text)
         st.subheader("The Response is:")
         st.write(response)
     else:
         st.write("Please upload the Resume")
-elif submit2:
+elif submit2 :
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt2,pdf_content,input_text)
+        response=get_gemini_response(option,input_prompt2,pdf_content,input_text)
         st.subheader("The Response is:")
         st.write(response)
     else:
         st.write("Please upload the Resume")
 
-# google_analytic_code=""" 
-# <head></head>
-# <!-- Google tag (gtag.js) -->
-# <script async src="https://www.googletagmanager.com/gtag/js?id=G-32PM6X2CNF"></script>
-# <script>
-#   window.dataLayer = window.dataLayer || [];
-#   function gtag(){dataLayer.push(arguments);}
-#   gtag('js', new Date());
 
-#   gtag('config', 'G-32PM6X2CNF');
-# </script>
+
+# input_prompt1= """
+# You are an experienced HR with Tech Expirence in the field of any one job role from Data Science, Full Stack Web Development, Big Data
+# Engineering, DEVOPS, Data Analyst, Data Visualisation, your task is to review the provided resume against the job
+# description for these profiles.
+# Please share your professional evaluation on whether the candidate's profile aligns with the role.
+# Higlight the strengths and weaknesses of the application in realtion to the specific job requirmnets.
 # """
-
-# st.html(google_analytic_code, unsafe_allow_html=True)
-# analytics = """
-# <!-- Google tag (gtag.js) -->
-# <script async src="https://www.googletagmanager.com/gtag/js?id=G-32PM6X2CNF"></script>
-# <script>
-#   window.dataLayer = window.dataLayer || [];
-#   function gtag(){dataLayer.push(arguments);}
-#   gtag('js', new Date());
-#   gtag('config', 'G-32PM6X2CNF');
-# </script>
+# input_prompt2= """
+# You are an skilled ATS (Application Tracking System) scanner with a deep understanding of any one job role Data Science, Full 
+# Stack Web Development, Big Data Engineering, DEVOPS, Data Analyst, Data Visualisation and deep ATS functionality,
+# your task is to review the provided resume against the job description for these profiles. Give the percentage match
+# if the resume matches the job description. 
+# First outcome should come as percentage and then the keyword missing,
+# Strength of the resume based in their skills in percentage. List the skills and how much relavant they are with JD,
+# Also point out the specifics words/lines/section/placing from the resume to change (if any),
+# top 5 things that candidate should add in his/her resume to get hired, 
+# 5 job description domain related projects to add to resume that might make candidate stand out 
+# and last the final thoughts. 
 # """
-
-# html(analytics)
-
-# st.write("""
-# <script async src="https://www.googletagmanager.com/gtag/js?id=G-32PM6X2CNF"></script>
-# <script>
-#   window.dataLayer = window.dataLayer || [];
-#   function gtag(){dataLayer.push(arguments);}
-#   gtag('js', new Date());
-#   gtag('config', 'G-32PM6X2CNF');
-# </script>
-# """, unsafe_allow_html=True)
